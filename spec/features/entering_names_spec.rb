050d9c95bfc10f0de25e52a458ce1c3da30470ea
Kynosaur <kye.bracey@gmail.com>
2017-05-24 15:44:49 +0100

    Add pretty headers and informative text to homepage

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -6,6 +6,6 @@ feature 'Entering names' do
     fill_in('Player1', with: "Kye")
     fill_in('Player2', with: "Toby")
     click_button('Submit')
-    expect(page).to have_content('Kye vs. Toby: BEGIN')
+    expect(page).to have_content('Kye vs. Toby: BEGIN!')
   end
 end
